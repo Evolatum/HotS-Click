76258b53879d8ff2f904c1c5ee1f1a4f9aaadc6b
Santiago MVO <santiago.mvao@gmail.com>
2020-02-02 19:52:40 -0600

    Updates

---
+++ precache-manifest.901fbc825bee0d3753440c11dda30257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae8f4a87b0dac0b8189bff294a1a81f",
+    "url": "/HotS-Click/index.html"
+  },
+  {
+    "revision": "2fcc3c2a5614aa630af9",
+    "url": "/HotS-Click/static/css/main.947919c3.chunk.css"
+  },
+  {
+    "revision": "82133795c0d7e27a232c",
+    "url": "/HotS-Click/static/js/2.8dca8a7d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/HotS-Click/static/js/2.8dca8a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fcc3c2a5614aa630af9",
+    "url": "/HotS-Click/static/js/main.729116d3.chunk.js"
+  },
+  {
+    "revision": "55ac7fd200ae69f78898",
+    "url": "/HotS-Click/static/js/runtime-main.621c7272.js"
+  }
+]);
